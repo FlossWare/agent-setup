e55a7edd23324d14437034183ff48473be35c67f
Scot P. Floess <scot.floess@gmail.com>
2026-08-25 22:55:54 -0400

    feat: expose complete config contract API

--- flossware_setup/config/__init__.py
+++ flossware_setup/config/__init__.py
@@ -1,6 +1,14 @@
 """Common, layered configuration contract and resolver."""
 
-from .resolver import ConfigResolver
+from .decorators import configurable, menu_item
+from .ordering import OrderingError, resolve_order
+from .policy import Policy, PolicyError
+from .registry import Component, ComponentRegistry
+from .resolver import ConfigLayer, ConfigResolver
 from .schema import ConfigSchema, ConfigField
 
-__all__ = ["ConfigResolver", "ConfigSchema", "ConfigField"]
+__all__ = [
+    "ConfigLayer", "ConfigResolver", "ConfigSchema", "ConfigField",
+    "Component", "ComponentRegistry", "Policy", "PolicyError",
+    "OrderingError", "resolve_order", "configurable", "menu_item",
+]

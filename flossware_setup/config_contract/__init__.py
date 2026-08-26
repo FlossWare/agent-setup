724423ec8692777f212e69d3220ec2fa71557d89
Scot P. Floess <scot.floess@gmail.com>
2026-08-25 23:07:24 -0400

    fix: establish non-conflicting config contract package

---
+++ flossware_setup/config_contract/__init__.py
@@ -0,0 +1,10 @@
+"""Language-neutral layered configuration contract."""
+
+from .decorators import configurable, menu_item
+from .ordering import OrderingError, resolve_order
+from .policy import Policy, PolicyError
+from .registry import Component, ComponentRegistry
+from .resolver import ConfigLayer, ConfigResolver
+from .schema import ConfigSchema, ConfigField
+
+__all__ = ["ConfigLayer", "ConfigResolver", "ConfigSchema", "ConfigField", "Component", "ComponentRegistry", "Policy", "PolicyError", "OrderingError", "resolve_order", "configurable", "menu_item"]
